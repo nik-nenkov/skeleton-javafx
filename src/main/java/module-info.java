ae20530ebae92cca247bdb196989361fa4e0a70a
Nikolay Nenkov <nik@nenkov.eu>
2024-01-16 15:53:53 +0200

    initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module eu.nenkov.skeletonjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens eu.nenkov.skeletonjavafx to javafx.fxml;
+    exports eu.nenkov.skeletonjavafx;
+}
